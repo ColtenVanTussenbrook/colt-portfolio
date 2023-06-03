925289f7f171497cd3dcd96f89725179da1eadb8
Colten Van Tussenbrook <coltenvantussenbrook@gmail.com>
2023-06-03 14:02:58 -0600

    set up layout

---
+++ src/app/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,9 @@
+import Nav from '../Nav/Nav'
+
+const Sidebar = () => (
+  <div className="flex flex-none flex-col justify-center bg-black w-1/6 fixed h-screen">
+    <Nav />
+  </div>
+)
+
+export default Sidebar
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,43 +3,31 @@ import Me from '../../public/images/me-circle.png'
 import GitHub from '../../public/images/github-logo.png'
 import Twitter from '../../public/images/twitter-logo.png'
 import LinkedIn from '../../public/images/linkedin-logo.png'
-import { Nav } from './components'
 
 const Home = () => {
   return (
-    <div className="flex">
-      <div className="flex flex-none flex-col justify-center bg-black w-1/6 fixed h-screen">
-        <Nav />
-      </div>
-      <div className="flex-auto">
-        <div className="flex justify-center items-center min-h-screen pl-40">
-          <div className="relative flex flex-col gap-2 items-center">
-            <Image width="200" src={Me} alt="photo of colten van tussenbrook" />
-            <p className="font-semibold text-5xl">
-              Hey, I&apos;m Colten Van Tussenbrook
-            </p>
-            <p className="text-lg">
-              I&apos;m a fullstack software engineer working mostly with React
-              and Node.js
-            </p>
-            <div className="flex gap-4">
-              <a href="https://github.com/ColtenVanTussenbrook">
-                <Image src={GitHub} alt="github logo" />
-              </a>
-              <a
-                className="pt-1"
-                href="https://www.linkedin.com/in/coltenvantussenbrook/"
-              >
-                <Image src={LinkedIn} alt="linkedin logo" />
-              </a>
-              <a href="https://twitter.com/coltvant">
-                <Image src={Twitter} alt="twitter logo" />
-              </a>
-            </div>
-            <div>Download Resume (PDF)</div>
-          </div>
-        </div>
-        <div>some other content</div>
+    <div className="relative flex flex-col gap-2 items-center">
+      <Image width="200" src={Me} alt="photo of colten van tussenbrook" />
+      <p className="font-semibold text-5xl">
+        Hey, I&apos;m Colten Van Tussenbrook
+      </p>
+      <p className="text-lg">
+        I&apos;m a fullstack software engineer working mostly with React and
+        Node.js
+      </p>
+      <div className="flex gap-4">
+        <a href="https://github.com/ColtenVanTussenbrook">
+          <Image src={GitHub} alt="github logo" />
+        </a>
+        <a
+          className="pt-1"
+          href="https://www.linkedin.com/in/coltenvantussenbrook/"
+        >
+          <Image src={LinkedIn} alt="linkedin logo" />
+        </a>
+        <a href="https://twitter.com/coltvant">
+          <Image src={Twitter} alt="twitter logo" />
+        </a>
       </div>
     </div>
   )
